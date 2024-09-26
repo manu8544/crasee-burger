0deb6c743e6a6256cf8a01d364d72ce5e1a4b703
Manuel Raguideau <manuel@opixido.com>
2024-09-26 19:19:36 +0200

    ♻️ F01: refacto

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 
 export default function LoginForm() {
 
-//State (état)
+  //State (état)
   const [inputValue, setInputValue] = useState('');
 
   //Comportement
   const handleSubmit = (event) => {
-      event.preventDefault();
-      alert(`Bonjour ${inputValue}`);
-      setInputValue('');  
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue('');  
   }
 
   const handleChange = (event) => {
@@ -19,12 +19,11 @@ export default function LoginForm() {
   //Affichage
   return (
     <form action="submit" onSubmit={handleSubmit}>
-            <h1>Bienvenue chez nous !</h1> 
-            <br />   
-            <h2>Connectez-vous</h2> 
-
-            <input type="text" value={inputValue} onChange={handleChange} required placeholder="Entrez votre prénom..."/>
-            <button>Accéder à votre espace</button>
-          </form> 
+      <h1>Bienvenue chez nous !</h1> 
+      <br />
+      <h2>Connectez-vous</h2> 
+      <input type="text" value={inputValue} onChange={handleChange} required placeholder="Entrez votre prénom..."/>
+      <button>Accéder à votre espace</button>
+    </form> 
   )
 }
