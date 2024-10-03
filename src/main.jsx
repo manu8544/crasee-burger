918553ed10d381207c124ce84d20d5cdf4077bc4
Manuel Raguideau <manuel@opixido.com>
2024-10-03 16:10:25 +0200

    ✨ F02: routing system + pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import OrderPage from './components/pages/order/OrderPage.jsx'
+import ErrorPage from './components/pages/error/ErrorPage.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/order/:username",
+    element: <OrderPage />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>  
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
