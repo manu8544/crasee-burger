d557a207b17941064d6f4b5fe351fc30748e732d
Manuel Raguideau <manuel@opixido.com>
2024-10-03 16:24:18 +0200

    ♻️ F02: refacto routing system with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import OrderPage from './components/pages/order/OrderPage.jsx'
-import ErrorPage from './components/pages/error/ErrorPage.jsx'
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/order/:username",
-    element: <OrderPage />,
-  }
-])
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>  
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
