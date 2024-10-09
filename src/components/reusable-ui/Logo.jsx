56bea9632a6ffaac4dcb1d9c285497eccc4bf5d6
Manuel Raguideau <manuel@opixido.com>
2024-10-09 18:44:15 +0200

    💄 F03: LOGIN > LoginPage > Logo > add styling

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,10 +1,39 @@
 import styled from "styled-components";
+import { theme } from "../../theme";
 
 export default function Logo() {
-  return <LogoStyled>Logo</LogoStyled>;
+  return (
+    <LogoStyled>
+      <h1>
+        <span>crazee</span>
+        <img src="/images/logo-orange.png" alt="" />
+        <span>burger</span>
+      </h1>
+    </LogoStyled>
+  );
 }
 
 const LogoStyled = styled.div`
-  background: blue;
-  // width:100%;
+  h1 {
+    color: ${theme.colors.primary};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+    /* transform: scale(2.5); */
+
+    span {
+      font-family: "Amatic SC", sans-serif;
+      font-weight: 700;
+      font-style: normal;
+      /* font-size: 36px; */
+      font-size: 110px;
+    }
+
+    img {
+      /* width: 80px; */
+      width: 200px;
+      margin: 0 20px;
+    }
+  }
 `;
