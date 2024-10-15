3d5f223746bbdbc60263798d3a9b71e30680ce22
Manuel Raguideau <manuel@opixido.com>
2024-10-15 11:40:54 +0200

    ✨ F05: ORDER > Main > Layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,8 +1,28 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import { fakeMenu1, fakeMenu2 } from "../../../fakeData/fakeMenu";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  console.log(fakeMenu2);
+
+  return (
+    <MainStyled>
+      <div className="liste-menus">
+        {fakeMenu2.map((item) => (
+          <div className="menu" key={item.id}>
+            <div className="menu-image">
+              <img src={item.imageSource} />
+            </div>
+            <div className="menu-titre">{item.title}</div>
+            <div className="menu-info">
+              <div className="menu-prix">{item.price}</div>
+              <button className="menu-bouton">Ajouter</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.div`
@@ -12,4 +32,28 @@ const MainStyled = styled.div`
   /* min-height: calc(100vh - 98px - 48px); */
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  .liste-menus {
+    display: grid;
+    /* grid-template: 50% 50% / 1fr 1fr 1fr 1fr; */
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    justify-items: center;
+
+    .menu {
+      background: green;
+      width: 240px;
+      margin: 20px 0;
+
+      .menu-image {
+        width: 200px;
+        height: 145px;
+
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+        }
+      }
+    }
+  }
 `;
