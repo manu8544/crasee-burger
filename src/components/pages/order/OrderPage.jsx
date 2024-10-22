11b815b7e21362c151bf5564eb48195338029afe
Manuel Raguideau <manuel@opixido.com>
2024-10-22 23:14:53 +0200

    💄 F07: ORDER > add styling adminTabs and buttons

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 import Navbar from "./Navbar/Navbar";
 import Main from "./Main/Main";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
 
 export default function OrderPage() {
   return (
@@ -11,9 +14,17 @@ export default function OrderPage() {
         <Main />
         <div className="adminPanel">
           <div className="adminTabs">
-            <button>Réduire</button>
-            <button>Ajouter un produit</button>
-            <button>Modifier un produit</button>
+            <button>
+              <FiChevronDown />
+            </button>
+            <button className="selected">
+              <AiOutlinePlus />
+              <span>Ajouter un produit</span>
+            </button>
+            <button>
+              <MdModeEditOutline />
+              <span>Modifier un produit</span>
+            </button>
           </div>
           <div className="adminContent">Contenu</div>
         </div>
@@ -41,21 +52,60 @@ const OrderPageStyled = styled.div`
     overflow: hidden;
 
     .adminPanel {
-      background: red;
       position: fixed;
       bottom: 0;
       left: 0;
       width: calc(100% - 48px);
       margin: 24px;
+      padding-top: 10px;
 
       border-bottom-left-radius: ${theme.borderRadius.extraRound};
       border-bottom-right-radius: ${theme.borderRadius.extraRound};
       overflow: hidden;
 
       .adminTabs {
-        background: green;
         padding: 0 0 0 70px;
-        height: 43px;
+        display: flex;
+
+        button {
+          height: 43px;
+          border: ${theme.colors.greyLight} solid 1px;
+          border-bottom: ${theme.colors.greyLight} solid 2px;
+          background: ${theme.colors.white};
+          box-shadow: ${theme.shadows.subtle};
+
+          border-top-left-radius: ${theme.borderRadius.round};
+          border-top-right-radius: ${theme.borderRadius.round};
+          margin-left: 1px;
+          padding: 0 20px;
+
+          font-family: "Open Sans", sans-serif;
+          font-size: ${theme.fonts.size.P0};
+          color: ${theme.colors.greySemiDark};
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          cursor: pointer;
+
+          span {
+            margin-left: 13px;
+          }
+
+          &.selected {
+            background: ${theme.colors.background_dark};
+            color: ${theme.colors.white};
+            border-color: ${theme.colors.background_dark};
+          }
+
+          &:hover {
+            text-decoration: underline;
+          }
+          &:hover:not(.selected) {
+            border-bottom-color: ${theme.colors.white};
+          }
+        }
       }
 
       .adminContent {
