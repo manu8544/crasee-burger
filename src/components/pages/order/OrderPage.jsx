198cdc8b7f9eb389182c091a764ded38113282f8
Manuel Raguideau <manuel@opixido.com>
2024-10-23 16:48:24 +0200

    ✨ F07: Add reusable Tab

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
 import Navbar from "./Navbar/Navbar";
-import Main from "./Main/Main";
+import Main from "./main/Main";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdModeEditOutline } from "react-icons/md";
+import Tab from "../../reusable-ui/Tab";
 
 export default function OrderPage() {
   return (
@@ -14,17 +15,9 @@ export default function OrderPage() {
         <Main />
         <div className="adminPanel">
           <div className="adminTabs">
-            <button>
-              <FiChevronDown />
-            </button>
-            <button className="selected">
-              <AiOutlinePlus />
-              <span>Ajouter un produit</span>
-            </button>
-            <button>
-              <MdModeEditOutline />
-              <span>Modifier un produit</span>
-            </button>
+            <Tab Icon={<FiChevronDown />} />
+            <Tab Icon={<AiOutlinePlus />} label={"Ajouter un produit"} className="selected" />
+            <Tab Icon={<MdModeEditOutline />} label={"Modifier un produit"} />
           </div>
           <div className="adminContent">Contenu</div>
         </div>
@@ -66,46 +59,6 @@ const OrderPageStyled = styled.div`
       .adminTabs {
         padding: 0 0 0 70px;
         display: flex;
-
-        button {
-          height: 43px;
-          border: ${theme.colors.greyLight} solid 1px;
-          border-bottom: ${theme.colors.greyLight} solid 2px;
-          background: ${theme.colors.white};
-          box-shadow: ${theme.shadows.subtle};
-
-          border-top-left-radius: ${theme.borderRadius.round};
-          border-top-right-radius: ${theme.borderRadius.round};
-          margin-left: 1px;
-          padding: 0 20px;
-
-          font-family: "Open Sans", sans-serif;
-          font-size: ${theme.fonts.size.P0};
-          color: ${theme.colors.greySemiDark};
-
-          display: flex;
-          align-items: center;
-          justify-content: center;
-
-          cursor: pointer;
-
-          span {
-            margin-left: 13px;
-          }
-
-          &.selected {
-            background: ${theme.colors.background_dark};
-            color: ${theme.colors.white};
-            border-color: ${theme.colors.background_dark};
-          }
-
-          &:hover {
-            text-decoration: underline;
-          }
-          &:hover:not(.selected) {
-            border-bottom-color: ${theme.colors.white};
-          }
-        }
       }
 
       .adminContent {
