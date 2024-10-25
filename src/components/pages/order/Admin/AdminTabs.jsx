3889adace9b902d002d2c560fdf3ce5191b21e18
Manuel Raguideau <manuel@opixido.com>
2024-10-26 00:52:45 +0200

    ♻️ F07: AdminTabs > selectTab in one function

--- src/components/pages/order/Admin/AdminTabs.jsx
+++ src/components/pages/order/Admin/AdminTabs.jsx
@@ -9,28 +9,26 @@ import OrderContext from "../../../../context/OrderContext";
 export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
   const { adminContent, setAdminContent } = useContext(OrderContext);
 
-  const [isActiveTab1, setIsActiveTab1] = useState(true);
-  const [isActiveTab2, setIsActiveTab2] = useState(false);
+  const [isAddSelected, setIsAddSelected] = useState(true);
+  const [isEditSelected, setIsEditSelected] = useState(false);
 
   const actionTogglePanel = () => {
     setIsCollapsed(!isCollapsed);
   };
 
-  const actionAddProduct = () => {
-    if (isActiveTab1) return;
-
-    setIsActiveTab1(!isActiveTab1);
-    setIsActiveTab2(false);
+  const selectTab = (tabSelected) => {
     setIsCollapsed(false);
-    setAdminContent("Ajouter un produit");
-  };
-  const actionEditProduct = () => {
-    if (isActiveTab2) return;
 
-    setIsActiveTab1(false);
-    setIsActiveTab2(!isActiveTab2);
-    setIsCollapsed(false);
-    setAdminContent("modifier un produit");
+    if (tabSelected === "add") {
+      setIsAddSelected(true);
+      setIsEditSelected(false);
+      setAdminContent("Ajouter un produit");
+    }
+    if (tabSelected === "edit") {
+      setIsAddSelected(false);
+      setIsEditSelected(true);
+      setAdminContent("modifier un produit");
+    }
   };
 
   return (
@@ -43,14 +41,14 @@ export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
       <Tab
         Icon={<AiOutlinePlus />}
         label={"Ajouter un produit"}
-        onClick={actionAddProduct}
-        className={isActiveTab1 ? "is-active" : ""}
+        onClick={() => selectTab("add")}
+        className={isAddSelected ? "is-active" : ""}
       />
       <Tab
         Icon={<MdModeEditOutline />}
         label={"Modifier un produit"}
-        onClick={actionEditProduct}
-        className={isActiveTab2 ? "is-active" : ""}
+        onClick={() => selectTab("edit")}
+        className={isEditSelected ? "is-active" : ""}
       />
     </AdminTabsStyled>
   );
