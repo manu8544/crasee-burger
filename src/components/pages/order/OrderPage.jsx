9856ffcd1ff86f729f6a4f3a255e6a0d66ac8cc2
Manuel Raguideau <manuel@opixido.com>
2024-11-04 18:40:53 +0100

    🐛 F07: update path Main.jsx

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
 import Navbar from "./Navbar/Navbar";
-import Main from "./main/Main";
+import Main from "./Main/Main";
 import OrderContext from "../../../context/OrderContext";
 import { useState } from "react";
 
