bf4b9caa8b352316f52e999388d72060ea11168b
Manuel Raguideau <manuel@opixido.com>
2024-11-04 15:28:53 +0100

    🔥 F07: ADMIN > Remove useless code for tests

--- src/components/pages/order/Admin/getTabsConfig.jsx
+++ src/components/pages/order/Admin/getTabsConfig.jsx
@@ -1,16 +1,7 @@
 import { AiOutlinePlus } from "react-icons/ai";
-import { BiSun } from "react-icons/bi";
-import { GiBaseballGlove } from "react-icons/gi";
-import { MdModeEditOutline, MdOutlineLocalDrink } from "react-icons/md";
+import { MdModeEditOutline } from "react-icons/md";
 
 export const getTabsConfig = () => [
-  // {
-  //   index:"",
-  //   Icon: isCollapsed ? <FiChevronDown /> : <FiChevronUp />,
-  //   label: "",
-  //   onClick: actionTogglePanel,
-  //   className: isCollapsed ? "is-active" : "",
-  // },
   {
     index: "add",
     label: "Ajouter un produit",
@@ -18,24 +9,9 @@ export const getTabsConfig = () => [
   },
   {
     index: "edit",
-    label: "Modifier un pr oduit",
+    label: "Modifier un produit",
     Icon: <MdModeEditOutline />,
   },
-  // {
-  //   index: "theme",
-  //   label: "Changer thème",
-  //   Icon: <BiSun />,
-  // },
-  // {
-  //   index: "drink",
-  //   label: "Boire un verre",
-  //   Icon: <MdOutlineLocalDrink />,
-  // },
-  // {
-  //   index: "baseball",
-  //   label: "HomeRun",
-  //   Icon: <GiBaseballGlove />,
-  // },
 ];
 
 export const getTabSelected = (tabs, currentTabSelected) => {
