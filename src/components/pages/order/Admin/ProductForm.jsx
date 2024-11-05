d44d5e7fb564e289041c120345c0bb8e6cc71f45
Manuel Raguideau <manuel@opixido.com>
2024-11-05 15:28:32 +0100

    💄 F08: ADMIN > ProductForm > add styling

--- src/components/pages/order/Admin/ProductForm.jsx
+++ src/components/pages/order/Admin/ProductForm.jsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
+import { theme } from "../../../../theme";
+import { FaHamburger } from "react-icons/fa";
+import { MdOutlineEuro } from "react-icons/md";
+import { BsFillCameraFill } from "react-icons/bs";
 
 export default function ProductForm() {
   return (
     <ProductFormStyled className="admin-product">
-      <div className="product-image">Image</div>
+      <div className="product-image">Aucune image</div>
 
       <div className="product-form">
         <div className="form-field">
+          <span className="field-icon">
+            <FaHamburger />
+          </span>
           <input
             type="text"
             name="product_name"
@@ -16,6 +23,9 @@ export default function ProductForm() {
         </div>
 
         <div className="form-field">
+          <span className="field-icon">
+            <BsFillCameraFill />
+          </span>
           <input
             type="text"
             name="product_img_link"
@@ -25,6 +35,9 @@ export default function ProductForm() {
         </div>
 
         <div className="form-field">
+          <span className="field-icon">
+            <MdOutlineEuro />
+          </span>
           <input type="text" name="product_price" id="product_price" placeholder="Prix" />
         </div>
 
@@ -37,18 +50,72 @@ export default function ProductForm() {
 const ProductFormStyled = styled.div`
   display: grid;
   grid-template-columns: minmax(80px, 215px) minmax(50%, 645px);
-  background: red;
+  gap: 20px;
 
   .product-image {
-    background: blue;
+    color: ${theme.colors.greySemiDark};
+    height: 120px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+
+    font-size: ${theme.fonts.size.P0};
+
+    border: ${theme.colors.greyLight} solid 1px;
+    border-radius: ${theme.borderRadius.round};
+
+    img {
+      object-fit: contain;
+      width: 100%;
+      height: 100%;
+    }
   }
 
   .product-form {
-    background: green;
-
+  
     .form-field {
+      display: flex;
+      align-items: center;
+      margin-bottom: 8px;
+      background: ${theme.colors.background_white};
+      border-radius: ${theme.borderRadius.round};
+
+      .field-icon {
+        margin: 0 13px;
+        color: ${theme.colors.greyBlue};
+        }
+      }
+
       input {
         width: 100%;
+        height: 35px;
+        border: none;
+        background: none;
+        font-size: ${theme.fonts.size.SM};
+
+        &::placeholder {
+          color: ${theme.colors.greyMedium};
+        }
+      }
+    }
+
+    button#product_submit {
+      background: ${theme.colors.success};
+      padding: 10px;
+      border-radius: ${theme.borderRadius.round};
+      border:${theme.colors.success} solid 1px;
+
+      color: ${theme.colors.white};
+      font-size: ${theme.fonts.size.XS};
+      font-weight: ${theme.fonts.weights.bold};
+
+      cursor: pointer;
+
+      &:hover {
+        background: ${theme.colors.white};
+        color: ${theme.colors.success};       
       }
     }
   }
